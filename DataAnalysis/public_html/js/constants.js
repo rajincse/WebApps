942ecc61b0b1a6c8da85072e0b96a17a71fef859
rajincse <rajin.cse@gmail.com>
2016-03-28 00:06:27 -0400

    #3
    Background lines added

--- DataAnalysis/public_html/js/constants.js
+++ DataAnalysis/public_html/js/constants.js
@@ -6,5 +6,5 @@ var labelRectWidth =300;
 var maxLabelChars = 50;
 var imageSVGWidth =500, 
     imageSVGHeight =500;
-var backgroundTimeStep =1000;
-var backgroundTimeMarker = 60000;
+var backgroundTimeStep =1000; // 1 sec
+var backgroundTimeMarker = 60000; // 1 min
