3726869da32673f68a3859541222658a29da4306
rajincse <rajin.cse@gmail.com>
2016-05-01 15:16:03 -0400

    #4 Project structure added

---
+++ ArchitectureDataAnalysis/web/js/handlers.js
@@ -0,0 +1,289 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var drag = d3.behavior.drag()
+                .on("dragstart", dragstarted)
+                .on("drag", dragged)
+                .on("dragend", dragended);
+var dragX =labelRectWidth;
+function dragstarted(d) {
+    d3.event.sourceEvent.stopPropagation();
+    d3.select(this).classed("dragging", true);
+  }
+
+  function dragged(d) {
+      dragX+= d3.event.dx;
+    d3.select(this).attr("transform", "translate(" +  dragX+",0)");
+  }
+
+  function dragended(d) {
+    d3.select(this).classed("dragging", false);
+  }
+
+var zoomListener = d3.behavior.zoom().scaleExtent([0.1, 3]).on("zoom", zoom);
+function zoom() {
+    d3.select('#base').attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+}
+var zoomListernerWithVerticalPanning = d3.behavior.zoom().scaleExtent([0.1, 3]).on("zoom", verticalPanningZoom);
+function verticalPanningZoom() {
+    d3.select('#base').attr("transform", "translate(0," + d3.event.translate[1] + ")scale(" + d3.event.scale + ")");
+}
+function initZoom()
+{
+    zoomListener.scale(1);
+    zoomListener.translate([0,0]);
+    zoomListernerWithVerticalPanning.scale(1);
+    zoomListernerWithVerticalPanning.translate([0,0]);
+}
+
+function onZoomIn()
+{
+    var newScale = Math.min(3, getScale()+0.1);
+    setScale(newScale);
+}
+
+function onZoomOut()
+{
+    var newScale = Math.max(0.1, getScale()-0.1);
+    setScale(newScale);
+}
+function getScale()
+{
+     var transform =d3.select('#base').attr("transform");
+     var regExp = /scale\([-]?\d*\.?\d+\)/;
+     if(regExp.test(transform))
+     {
+         var scaleComamnd = transform.match(regExp)[0];
+         var scaleValue = parseFloat(scaleComamnd.match(/[-]?\d*\.?\d+/)[0]);
+         return scaleValue;
+     }
+     return 1.0;
+}
+function setScale(newScale)
+{
+    var transform =d3.select('#base').attr("transform");
+    var regExp = /scale\([-]?\d*\.?\d+\)/;
+    var transformText ='';
+    if(regExp.test(transform))
+    {
+        transformText = transform.replace(regExp, 'scale('+newScale+')');    
+    }
+    else
+    {
+        transformText ='scale('+newScale+')';
+    }
+    d3.select('#base').attr("transform", transformText);
+}
+var selectedIndex =-1;
+function onMouseOver()
+{
+    var currentElementId = d3.select(this).attr('id');
+    var index = currentElementId.split('-');
+    var hoverId = 'hover-'+index[1];
+    d3.select('#'+hoverId).style('visibility','visible');
+}
+function onMouseOut()
+{
+    var currentElementId = d3.select(this).attr('id');
+    var index = currentElementId.split('-');
+    if(index[1] != selectedIndex)
+    {
+        var hoverId = 'hover-'+index[1];
+        d3.select('#'+hoverId).style('visibility','hidden');
+    }
+    
+}
+function onSvgClick(d,i)
+{
+    d3.select('.selected-content')
+            .style('visibility','hidden');
+    d3.select('#hover-'+selectedIndex).style('visibility','hidden');
+    d3.select('.additional-properties-container').style('visibility','hidden');
+    selectedIndex =-1;
+}
+function refreshAdditionalPropertiesView(id)
+{
+    console.log('refresh');
+    d3.json(dataServletGetElementProperties+id, 
+                function(error, data)
+                {
+                    var container = d3.select('.additional-properties-container div.all-properties');
+                    var divs = container.selectAll('div').data(data.DataArray)
+                            .enter()
+                            .append('div')
+                            .attr('class','row');
+                    
+                    divs.append('div')
+                            .attr('class','cell label')                            
+                            .text(function(d){ return d.property_name+': ';});
+                    divs.append('div')
+                            .attr('class','cell')
+                            .text(function(d){ return d.property_value;});     
+                }
+            );
+    d3.json(dataServletGetAllProperties, 
+                function(error, data)
+                {
+                    var selectEnter = d3.select('.propertyList')
+                                    .selectAll('option').data(data.DataArray)
+                                    .enter();
+                    selectEnter.append('option')
+                            .attr('value', function(d){ return d.id;})
+                            .text(function(d){ return d.property_name; })
+                        ;
+                            
+                }
+            );
+}
+function onAdditionalClick(id, x, y)
+{
+   
+    var dialogBox = d3.select('.additional-properties-container');
+    var visibility = dialogBox.style('visibility');
+    var newVisibility  = (visibility === 'hidden'? 'visible':'hidden');
+    dialogBox.style('visibility',newVisibility)
+            .style('left',x+'px')
+            .style('top', y+'px')            
+        ;
+    refreshAdditionalPropertiesView(id);   
+        
+        
+        d3.select('#addProperty')
+                .on('click', function()
+                    {
+                        var propertyName = d3.select('#propertyName').node().value;
+                        var url = 'DataServlet?method=addNewProperty&propertyName='+propertyName;
+                        d3.json(url, function(error, data)
+                        {
+                           refreshAdditionalPropertiesView(id); 
+                        });
+                    });
+        d3.select('#addPropertyValue')
+                .on('click', function()
+                    {
+                        var propertyId = d3.select('.propertyList').node().value;
+                        var propertyValue = d3.select('#propertyValue').node().value;
+                        
+                        var url = 'DataServlet?method=addNewPropertyValue'
+                                        +'&propertyId='+propertyId
+                                        +'&elementId='+id
+                                        +'&propertyValue='+propertyValue;
+                        d3.json(url, function(error, data)
+                        {
+                           refreshAdditionalPropertiesView(id);
+                        });
+                    });
+        
+}
+function onClick(d, i)
+{
+    d3.event.stopPropagation();
+    selectedIndex = i;
+    
+    var x = d3.event.pageX;
+    var y = d3.event.pageY;
+    d3.select('.selected-content')
+            .style('visibility','visible')
+            .style('left',d3.event.pageX+'px')
+            .style('top', d3.event.pageY+'px')
+        ;
+    
+
+    var svgWidth = d3.select('.media-image-svg').attr('width');
+
+
+    var selectedObject =inputdb.getContent(d.dataObject.label);
+
+    d3.select('.id-container').text(d.dataObject.label);
+    d3.select('.type').text(selectedObject.type);
+    d3.select('.type-text').text(selectedObject.typeText);
+    d3.select('.navigation-text').text(selectedObject.navigationText);
+    
+    var atIndex = d.dataObject.label.indexOf('@');
+    var strId = d.dataObject.label.substring(atIndex+1, d.dataObject.label.length);
+    d3.select('#additionalPropertyClick')
+                    .on('click', function ()
+            {
+                    onAdditionalClick(strId, x+500, y);
+            });
+   
+
+    
+
+    if(selectedObject.type ==='image'
+            ||selectedObject.type ==='button')
+    {
+        d3.select('.data-content').text('Image:')
+        d3.select('.data-text').text('');
+
+        var imagePath = 'input'+selectedObject.image.path;
+        d3.select('.media-image-svg').selectAll("*").remove();
+        var ratio = 1.0 * svgWidth /selectedObject.image.dimension[0];
+
+        d3.select('.media-image-svg')
+                .attr('height', imageSVGHeight)
+                .append('svg:image')
+                .attr('xlink:href', imagePath)
+                .attr('x',0)
+                .attr('y',0)                            
+                .attr('width',selectedObject.image.dimension[0] * ratio)
+                .attr('height', selectedObject.image.dimension[1] * ratio)
+        ;
+
+    }
+    else if(selectedObject.type ==='aoi')
+    {
+
+        d3.select('.data-content').text('Image:')
+        d3.select('.data-text').text('');
+        var ratio = 1.0 * svgWidth /selectedObject.image.dimension[0];
+
+
+        d3.select('.media-image-svg').selectAll("*").remove();
+        var imagePath = 'input'+selectedObject.image.path;
+        d3.select('.media-image-svg')
+                .attr('height', imageSVGHeight)
+                .append('svg:image')
+                .attr('xlink:href', imagePath)
+                .attr('x',0)
+                .attr('y',0)
+                .attr('width',selectedObject.image.dimension[0] * ratio)
+                .attr('height', selectedObject.image.dimension[1] * ratio)
+        ;
+        if(selectedObject.aoi)
+        {
+            d3.select('.media-image-svg')
+                    .append('rect')
+                    .attr('class','aoi-rect')
+                    .attr('x',selectedObject.aoi.x* ratio)
+                    .attr('y',selectedObject.aoi.y* ratio)
+                    .attr('width',selectedObject.aoi.width* ratio)
+                    .attr('height', selectedObject.aoi.height* ratio);
+            //Title of aoi
+//            d3.select('.media-image-svg')
+//                    .append('text')
+//                    .text(selectedObject.aoi.name)
+//                    .attr('x',selectedObject.aoi.x* ratio)
+//                    .attr('y',(selectedObject.aoi.y +20)* ratio)
+//                    .attr('font-family',"Verdana")
+//                    .attr('font-size',20 *ratio)
+//                    .attr('fill','blue')
+//            ;
+        } 
+    }
+    else if(selectedObject.type ==='text')
+    {
+        d3.select('.media-image-svg').attr('height', 0).selectAll("*").remove();
+        var text = selectedObject.object;
+        if(selectedObject.object.text)
+        {
+            text = selectedObject.object.text;
+        }
+        d3.select('.data-content').text('Text:');
+        d3.select('.data-text').text(text);
+    }
+}
---
+++ ArchitectureDataAnalysis/web/js/inputdb.js
@@ -0,0 +1,309 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var inputdb=
+{
+    baseDataPath :"inputdata/base_data.json",
+    categories: [],
+    load: function()
+    {
+         d3.json(this.baseDataPath, function (error, data) {
+             var categoryKeys = Object.keys(data.categories);
+             for(var categoryIndex =0;categoryIndex < categoryKeys.length;categoryIndex++)
+             {
+                 var category = data.categories[categoryKeys[categoryIndex]];
+                 
+                 inputdb.categories[categoryIndex] = category;
+                 inputdb.categories[categoryIndex].modules=[];
+                 
+                 var moduleKeys = Object.keys(inputdb.categories[categoryIndex].subCategory);
+                 
+                 for(var moduleIndex =0;moduleIndex < moduleKeys.length;moduleIndex++)
+                 {
+                     var moduleDataFilePaths = inputdb.categories[categoryIndex].subCategory[moduleKeys[moduleIndex]].dataFile;
+                     
+                     d3.json('input'+moduleDataFilePaths, function (error, data)
+                     {
+                         var indices = data.id.split(/\D/).clean(''); // example id c0m1 => category 0 , module 1
+                         var dataCategoryIndex = indices[0]; 
+                         var dataModuleIndex = indices[1];
+                         var category =inputdb.categories[dataCategoryIndex];
+                         category.modules[dataModuleIndex] = data;
+                        
+                         
+                     });
+                 }
+             }
+             
+             
+             
+         });
+         
+    },    
+    getContent : function(id)
+    {
+         var colonIndex = id.indexOf(':'); 
+         var atIndex = id.indexOf('@');
+         var strId = id.substring(atIndex+1, id.length);
+         var type=id.substring(colonIndex+1, atIndex);
+         
+         var keys = strId.split(/\d/).clean('');
+         var values = strId.split(/\D/).clean('');
+         var objectId={navigationText:'' };
+         
+         
+         for(var i=0;i<keys.length;i++)
+         {
+             this.setItem(keys[i], values[i], objectId)
+             
+         }
+         var selected = {"object":objectId.current.object, 
+             "typeText":objectId.current.typeText, 
+             "type":type, 
+             'navigationText':objectId.navigationText,
+             'image':objectId.image,
+             'aoi':objectId.aoi
+         };
+         
+         console.log(id+":"+JSON.stringify(selected));
+         return selected;
+         
+    },
+    getLabel: function(id)
+    {
+       
+       var content = this.getContent(id);
+       var colonIndex = id.indexOf(':'); 
+       var name = id.substring(0,colonIndex);
+       var suffix = "("+content.typeText+")";
+       var label =name+suffix;
+       if(label.length > maxLabelChars)
+       {
+           label = name.substring(0,maxLabelChars-suffix.length-3)+"..."+suffix;
+       }
+       return label;
+    },
+    setItem : function( key, value, object)
+    {
+       
+        
+        if(key === 'c') //category
+        {
+            object.current = { object: this.categories[value] , parent:{}};
+            object.currentKey =key;
+            object.navigationText+= 'category:'+ object.current.object.name;
+        }
+        else if(key === 'p')
+        {
+            if(object.currentKey ==='c')
+            {
+                object.current ={ 
+                            object:{ 
+                                        'title': object.current.object.name,
+                                        'text': object.current.object.description
+                                    }
+                            ,parent:object.current
+                            }
+                        ;
+                object.current.typeText = 'Category Description';
+                object.navigationText+= '=> Category Description';
+            }
+            else if(object.currentKey ==='m')
+            {
+                 object.current ={ 
+                            object:object.current.object.description
+                            ,parent:object.current
+                            }
+                        ;
+                 object.current.typeText = 'Module Description';
+                 object.navigationText+= '=> Module Description';
+            }
+            else if(object.currentKey ==='s')
+            {
+                object.current ={ 
+                            object:object.current.object.description[value]
+                            ,parent:object.current
+                            }
+                        ;
+                object.current.typeText = 'SubModule Description';
+                object.navigationText+= '=> SubModule Description';
+            }
+            object.currentKey =key;
+        }
+        else if(key === 'h')
+        {
+            if(object.currentKey === 'p')
+            {
+                object.current ={ 
+                            object:object.current.object.title
+                            ,parent:object.current
+                            }
+                        ;
+                object.currentKey =key;    
+                object.current.typeText = 'Paragraph heading';
+                object.navigationText+= '=> Heading:'+object.current.object;
+            }
+            else if(object.currentKey === 'm')
+            {
+                object.current ={ 
+                                object:object.current.object.moduleName
+                                ,parent:object.current
+                            }
+                        ;
+                object.currentKey =key;    
+                object.current.typeText = 'Module Name';
+                object.navigationText+= '=> Module Name:'+object.current.object;
+            }
+            else if(object.currentKey === 's')
+            {
+                object.current ={ 
+                                object:object.current.object.subModuleName
+                                ,parent:object.current
+                            }
+                        ;
+                object.currentKey =key;    
+                object.current.typeText = 'Submodule Name';
+                object.navigationText+= '=> Submodule Name:'+object.current.object;
+            }
+            else if(object.currentKey === 'g')
+            {
+                object.current ={ 
+                                object:'HomeLogo Text'
+                                ,parent:object.current
+                            }
+                        ;
+                object.currentKey =key;    
+                object.current.typeText = 'HomeLogo Text';
+                object.navigationText+= '=> HomeLogo Text';
+            }
+            
+        }
+        else if(key === 'm')
+        {
+            if(object.currentKey==='c')
+            {
+                object.current ={ 
+                                object:object.current.object.modules[value]
+                                ,parent:object.current
+                            }
+                        ;
+                object.currentKey =key;
+                object.current.typeText = 'Module';
+                object.navigationText+= '=> Module:'+object.current.object.moduleName;
+            }
+            
+            
+        }
+        else if(key === 's')
+        {
+            object.current ={ 
+                                object:object.current.object.submodule[value]
+                                ,parent:object.current
+                            }
+                        ;
+            object.currentKey =key;
+            object.current.typeText = 'Submodule';
+            object.navigationText+= '=> Submodule:'+object.current.object.subModuleName;
+        }
+        else if(key === 'i')
+        {
+            if(object.currentKey==='s')
+            {
+                object.current ={ 
+                                object:object.current.object.mediaList[value]
+                                ,parent:object.current
+                            }
+                        ;
+                object.current.typeText = 'Media Image';
+                object.navigationText+= '=> Media Image';
+                object.image ={path:object.current.object.mediaImage, 
+                                dimension:[object.current.object.aoiData.width,object.current.object.aoiData.height] };
+            }
+            object.currentKey =key;
+        }
+        else if(key === 'a')
+        {
+            if(object.currentKey==='i')
+            {
+                object.current ={ 
+                                object:{
+                                    imageName: object.current.object.aoiData.imageName,
+                                    aoi: object.current.object.aoiData.aoiItemList[value]
+                                }
+                                ,parent:object.current
+                            }
+                        ;
+                object.current.typeText = 'AOI';
+                object.navigationText+= '=> AOI';
+                object.image ={path:object.current.parent.object.mediaImage, 
+                                dimension:[object.current.parent.object.aoiData.width,object.current.parent.object.aoiData.height] };
+               
+                object.aoi =object.current.object.aoi;
+            }
+            object.currentKey =key;
+        }
+        else if(key === 't')
+        {
+            if(object.currentKey==='s')
+            {
+                object.current ={ 
+                                object:object.current.object.mediaList[value]
+                                ,parent:object.current
+                            }
+                        ;
+                object.current.typeText = 'Media Button';
+                object.navigationText+= '=> Media Button';
+                object.image ={path:object.current.object.mediaImage, 
+                                dimension:[object.current.object.aoiData.width,object.current.object.aoiData.height] };
+            }
+            object.currentKey =key;
+        }
+        else if(key === 'ai')
+        {
+            if(object.currentKey==='s')
+            {
+                var mediaIndex=-1;
+                for(var i=0;i<object.current.object.mediaList.length;i++)
+                {
+                    if(object.current.object.mediaList[i].additionalMedia)
+                    {
+                        mediaIndex =i;
+                        break;
+                    }
+                }
+                object.current ={ 
+                                object:object.current.object.mediaList[mediaIndex].additionalMedia[value]
+                                ,parent:object.current
+                            }
+                        ;
+                object.current.typeText = 'Additional Media';
+                object.navigationText+= '=> Additional Media';
+                object.image ={path:object.current.object, 
+                                dimension:[object.current.parent.object.mediaList[mediaIndex].aoiData.width,object.current.parent.object.mediaList[mediaIndex].aoiData.height]  };
+            }
+            object.currentKey =key;
+        }
+        else if(key =='g')
+        {
+            object.current = { object: 'global' , parent:{}};
+            object.currentKey =key;
+            object.navigationText+= 'global';
+        }
+        
+         
+    }
+    
+            
+};
+Array.prototype.clean = function(deleteValue) {
+  for (var i = 0; i < this.length; i++) {
+    if (this[i] === deleteValue) {         
+      this.splice(i, 1);
+      i--;
+    }
+  }
+  return this;
+};
+
