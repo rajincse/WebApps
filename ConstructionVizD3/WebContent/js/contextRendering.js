8d367e4c69d919fefcbc617ca08e89f3f4c15f4e
rajincse <rajin.cse@gmail.com>
2016-11-15 16:08:21 -0500

    #5
    Focus is populated

--- ConstructionVizD3/WebContent/js/contextRendering.js
+++ ConstructionVizD3/WebContent/js/contextRendering.js
@@ -1,13 +1,13 @@
 function contextRendering()
 {
 	var svgWidth =960;
-	var svgHeight = 500;
+	var svgHeight = 550;
 	var svg =d3.select('svg');
 	
 	svg.attr('width',svgWidth);
 	svg.attr('height',svgHeight);
 	
-	var marginFocus = {top: 20, right: 20, bottom: 110, left: 40},
+	var marginFocus = {top: 20, right: 20, bottom: 160, left: 40},
     marginContext = {top: 430, right: 20, bottom: 30, left: 40},
     width = +svg.attr("width") - marginFocus.left - marginFocus.right,
     heightFocus = +svg.attr("height") - marginFocus.top - marginFocus.bottom,
@@ -52,7 +52,7 @@ function contextRendering()
 						return getTimestamp(key);
 					})
 		);
-	var maxY = 1000;
+	var maxY = 100;
 	yScaleFocus.domain([maxY,0]);
 	
 	xScaleContext.domain(xScaleFocus.domain());
@@ -84,21 +84,121 @@ function contextRendering()
      .call(zoomFocus);
 	 
 	 
-	 var imageWidth =32;
-	 var imageHeight = 32;
-	 var padding = 8;
+	 var imageWidth =16;
+	 var imageHeight = 16;
+	 var padding = 0;
 	 var imageAreaWidth = imageWidth+padding;
 	 var imageAreaHeight =imageHeight+padding;
 	 
 	 //Focus Region 
 	 function renderFocus()
 	 {
+		 d3.select(focus).node().selectAll('.image-area').remove();
 		 console.log('Rendering focus');
+		 var minXFocus = xScaleFocus.range()[0];
+		 var maxXFocus = xScaleFocus.range()[1];
+		 
+		 var minTimeFocus = xScaleFocus.domain()[0];
+		 var maxTimeFocus = xScaleFocus.domain()[1];
+		 
+		 var timeInterval = imageAreaWidth*( maxTimeFocus - minTimeFocus)/ (maxXFocus - minXFocus);
+		 
+		 var mashedData=getMashedupDataRange(mainData, timeInterval,xScaleFocus.domain() );
+		 
+		 var averageCameraDistanceData ={};
+		 
+		 for(var i = 0; i< Object.keys(mashedData).length;i++)
+		 {
+			 var key = Object.keys(mashedData)[i];
+			 var value = mashedData[key];
+	 		var nameMap={};
+	 		for(var j=0;j<value.length;j++)
+	 		{
+	 			var viewedObject = value[j];
+	 			var name = viewedObject.name;
+	 			if(!nameMap[name])
+				{
+	 				nameMap[name] = [0, 0];
+				}
+	 			var cameraDistance = parseFloat(viewedObject.cameraDistance);
+	 			nameMap[name][0] += cameraDistance;
+	 			nameMap[name][1]++;
+	 		}
+	 		var maxOccurredName ="";
+	 		var maxOccurrence=0;
+	 		for(var k=0;k< Object.keys(nameMap).length;k++)
+	 		{
+	 			var name = Object.keys(nameMap)[k];
+	 			var occurrence = nameMap[name][1];
+	 			if(occurrence> maxOccurrence && nameMap[name][0] > 0)
+	 			{
+	 				maxOccurrence = occurrence;
+	 				maxOccurredName = name;
+	 			}
+	 		}
+	 		averageCameraDistanceData[key]={};
+	 		averageCameraDistanceData[key][maxOccurredName]=nameMap[maxOccurredName];
+		 }
+				 
+		 
+		 var averageCameraDistanceKeys = Object.keys(averageCameraDistanceData);
+		 
+		 
+		 var imageGroup = focus.append("g")
+			.attr('class', 'image-area')
+			;
+		 
+		 var timestampGroup= imageGroup.selectAll('g')
+			.data(averageCameraDistanceKeys).enter()
+			.append('g')
+			.attr('id', function(key){ return 'T-'+key;});
+		
+		 timestampGroup.selectAll('image')		
+			.data(function(key){ return Object.keys(averageCameraDistanceData[key]);})
+		 .enter()
+			.append('image')
+			.attr('id', function(name){
+				var timestamp = d3.select(this.parentNode).datum();
+				var cameraDistanceData = averageCameraDistanceData[timestamp][name];
+				var average = cameraDistanceData[0]/ cameraDistanceData[1];
+				return "c-"+average;
+			})
+			.attr('href', function(name)
+					{						
+						return imageData[name];
+					})
+					.attr('width', imageWidth)
+			.attr('height', imageHeight)
+			.attr('x', function(name)
+					{
+						var timestamp = d3.select(this.parentNode).datum();
+						var x =( timestamp*maxXContext / maxTimeContext);
+						
+						return x-imageAreaWidth/2;
+					})
+			.attr('y', function(name)
+			{
+				var timestamp = d3.select(this.parentNode).datum();
+				var cameraDistanceData = averageCameraDistanceData[timestamp][name];
+				
+				
+				var y = (cameraDistanceData[0]/ cameraDistanceData[1])*heightContext /maxY;
+				
+				
+				return  heightContext - y-imageAreaHeight/2;
+			})
+			.append('title')
+				.text(function(name)
+						{
+							return "name:"+name;
+						})
+						
+			;
 	 }
 	 
 	 //Context Region
 	 
-	 var tickInterval = xScaleContext.ticks()[1] -xScaleContext.ticks()[0];
+	 
 	 var maxXContext = xScaleContext.range()[1];
 	 var maxTimeContext = xScaleContext.domain()[1];
 	 var timeInterval = imageAreaWidth* maxTimeContext/ maxXContext;
@@ -171,7 +271,7 @@ function contextRendering()
 					var timestamp = d3.select(this.parentNode).datum();
 					var x =( timestamp*maxXContext / maxTimeContext);
 					
-					return x;
+					return x-imageAreaWidth/2;
 				})
 		.attr('y', function(name)
 		{
@@ -182,7 +282,7 @@ function contextRendering()
 			var y = (cameraDistanceData[0]/ cameraDistanceData[1])*heightContext /maxY;
 			
 			
-			return heightContext - y-imageAreaHeight/2;
+			return  heightContext - y-imageAreaHeight/2;
 		})
 		.append('title')
 			.text(function(name)
@@ -236,4 +336,37 @@ function contextRendering()
 		 }
 		return mashedData;
 	}
+	
+	function getMashedupDataRange(data, interval, range)
+	{
+		var keys = Object.keys(data);
+		var mashedData={};
+		for(var i =0;i< keys.length; i++)
+		 {
+			 var key = keys[i];
+			 var timestamp = getTimestamp(key);
+			 
+			 if(range)
+			 {
+				 if(timestamp < range[0]) continue;
+				 if(timestamp > range[1]) break;
+			 }
+			 
+			 
+			 var dataArray =  JSON.parse(data[key]);
+			 var mashedKey = Math.floor(timestamp/interval) * interval;
+			 
+			 if(mashedData[mashedKey])
+			 {
+				 var previousArray =mashedData[mashedKey];
+				 previousArray.push(dataArray);
+			 }
+			 else
+			 {
+				 mashedData[mashedKey]= dataArray;
+			 }
+		 }
+		return mashedData;
+	}
+	
 }
