2fd2ccd764853db46c8ccdc409c4ac2900468e4a
Sayeed Alam <saalam@microsoft.com>
2023-02-27 15:01:37 -0800

    Added image thubnail

--- React/reactplay/src/TileGallery/TileGalleryItem.jsx
+++ React/reactplay/src/TileGallery/TileGalleryItem.jsx
@@ -1,9 +1,12 @@
+import { ImageIcon } from "@fluentui/react";
+import MetricsChartTile from "../svg/MetricsChartTile.svg"
+
 export const TileGalleryItem = (props) => {
 
     return (
         <div className="tileGalleryItem">
             <div className="ext-tilegallery-thumbnail msportalfx-svg-palette-blue">
-                <img alt="" src="https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE1Mu3b?ver=5c31" />
+                <ImageIcon imageProps={ {src: MetricsChartTile} } />
             </div>
             <div className="ext-tilegallery-wrapper">
                 <span title={props.title}>{props.title}</span>
